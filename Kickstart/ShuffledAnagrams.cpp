1d6cd51d62d40c130e7453e02d4135ff7b0bc3e9
Utkarsh Mishra <anindiangeek@outlook.com>
2021-08-26 12:24:10 +0530

    Added KickStart & Other folder

---
+++ Kickstart/ShuffledAnagrams.cpp
@@ -0,0 +1,37 @@
+/*
+	Problem: couldn't solve : https://codingcompetitions.withgoogle.com/kickstart/round/000000000043585c/000000000085a152#problem
+	author : anindiangeek
+*/
+
+/* - - - - - - - - - - header/namespaces/defines - - - - - - - - - - */
+#include <bits/stdc++.h>
+using namespace std;
+/* - - - - - - - - - - header/namespaces/defines - - - - - - - - - -*/
+
+void solve()
+{
+	string s;
+	cin >> s;
+}
+
+/*- - - - - - - - - - - main() starts here - - - - - - - - - - */
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("C://Users//anind//Documents//CP//i.txt", "r", stdin);
+	freopen("C://Users//anind//Documents//CP//o.txt", "w+", stdout);
+	freopen("C://Users//anind//Documents//CP//err.txt", "w+", stderr);
+#else
+//nothing
+#endif
+	int64_t testcases = 1;
+	cin >> testcases;
+	while (testcases--)
+		solve();
+	return 0;
+}
+
+/* - - - - - - - - - - - End of the code - - - - - - - - - - */
