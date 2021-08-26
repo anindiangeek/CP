48b999fde43235e54de846a7226deac817cc0f72
Utkarsh Mishra <anindiangeek@outlook.com>
2021-08-26 11:47:01 +0530

    Edited Max function

--- Other Data, Templates, etc/Maximum & Minimum number.cpp
+++ Other Data, Templates, etc/Maximum & Minimum number.cpp
@@ -4,20 +4,12 @@ T maximum(T a, T b, T c = 0)
     if (a > b)
     {
         if (a > c)
-        {
             return a;
-        }
         else
-        {
             return c;
-        }
     }
     else if (b > c)
-    {
         return b;
-    }
     else
-    {
         return c;
-    }
 }
